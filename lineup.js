96baed46c86a3353337f27460234dcc9628ebde6
Samantha Abrams <sabramse@gmail.com>
2016-06-25 01:57:03 -0500

    random lineup generator

---
+++ lineup.js
@@ -0,0 +1,24 @@
+
+var randomFirsts = ["Matt Carpenter", "Kolten Wong", "Older David Eckstein"];
+var randomSeconds = ["Almedys Diaz", "Matthew Leach", "Brian Cobb"];
+var randomThirds = ["Beyonce", "Samantha Abrams", "That Guy at Glass Nickel"];
+var randomFourths = ["Matt Holliday", "Jhonny Peralta", "Johnny Peralta"];
+var randomFifths = ["Yadier Molina", "Jose Molina", "Benji Molina"];
+var randomSixths = ["Alex Rodriguez", "Alexander Hamilton", "Aaron Burr"];
+var randomSevenths = ["Mike Matheny", "Tony La Russa", "Brian's Dad"]
+var randomEighths = ["Do we have anyone left?", "Tommy Pham", "Stephen Piscotty"];
+var randomNinths = ["Adam Wainwright", "Roger Clemens", "Clayton Kershaw"];
+
+var randomFirst = randomFirsts[Math.floor(Math.random() * 3)];
+var randomSecond = randomSeconds[Math.floor(Math.random() * 3)];
+var randomThird = randomThirds[Math.floor(Math.random() * 3)];
+var randomFourth = randomFourths[Math.floor(Math.random() * 3)];
+var randomFifth = randomFifths[Math.floor(Math.random() * 3)];
+var randomSixth = randomSixths[Math.floor(Math.random() * 3)];
+var randomSeventh = randomSevenths[Math.floor(Math.random() * 3)];
+var randomEighth = randomEighths[Math.floor(Math.random() * 3)];
+var randomNinth = randomNinths[Math.floor(Math.random() * 3)];
+
+var randomLineup = randomFirst + ", " + randomSecond + ", " + randomThird + ", " + randomFourth + ", " + randomFifth + ", " + randomSixth + ", " + randomSeventh + ", " +  randomEighth + ", " + randomNinth;
+
+randomLineup;
